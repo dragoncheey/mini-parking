0221a51550a2ced6dcace89eb525727fe68b25e8
young.qian <309595222@qq.com>
2026-06-18 20:57:33 +0800

    feat(app): 初始化微信云开发环境
    - 在应用启动时初始化微信云环境
    - 设置环境ID为 prod-d0g1zp7w0322a5d42
    - 开启用户访问数据追踪功能

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 App({
+  onLaunch() {
+    if (wx.cloud) {
+      wx.cloud.init({
+        env: "prod-d0g1zp7w0322a5d42",
+        traceUser: true
+      });
+    }
+  },
   globalData: {
     userLocation: null,
     destination: null,
